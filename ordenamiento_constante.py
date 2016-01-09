1c91aa5eb67cb3af93a0861cdaf49c451a5e974d
Felipe Contreras <hobber.mallow@gmail.com>
2016-01-09 14:17:28 -0600

    saqué las listas de las definiciones de funciones a fin de que no influyan en sus tiempos

--- ordenamiento_constante.py
+++ ordenamiento_constante.py
@@ -8,21 +8,28 @@ procedimientos, la teoría nos dice que funcionará en tiempo CONSTANTE, lo cual
 menos que un algoritmo podría tardar, asintóticamente hablando. Aunque aquí hay por
 lo menos un problema. ¿Puedes identificar cuál es?. Nota: B es la lista A ordenada."""
 
-def algoritmo1():
-    "Super-chafa, pero ¿por qué funciona?."
+def algoritmo1(A):
+    "Super-chafa, pero ¿por qué funciona?, ¿cuánto tarda?."
     """ Pythonadas:
             [0] * 100 : genera una lista de 100 valores, todos iguales a '0'
+
+            for a in L:  para cada elemento 'a' de la lista (iterable) L, haz el ciclo
+
+            A[:100] : primeros 100 elementos de la lista A
     """
-    A = list(range(100))
-    random.shuffle(A)
-    print("A:", A)
     B = [0] * 100
-    for a in A:
+    for a in A[:100]:
         B[a] = a
     print("B:", B)
 
-def algoritmo2():
-    "Este mejora un poco, pero aún chafea, ¿por qué?, ¿cuánto se tarda?."
+print("Algoritmo 1")
+A = list(range(100))
+random.shuffle(A)
+print("A:", A)
+algoritmo1(A)
+
+def algoritmo2(A):
+    "Esto mejora un poco, pero aún chafea, ¿por qué?, ¿cuánto se tarda?."
     """ Pythonadas:
             randrange(n) : es como el range(n), pero aleatorio, es decir, genera
             números enteros al azar en el rango de 0 a n-1.
@@ -31,14 +38,8 @@ def algoritmo2():
             final de una linea ('\n'=cambio de línea) por la cadena que se le ponga
             al 'end', en este caso, una coma y un espacio SIN CAMBIO DE LINEA.
     """
-    A = []
-    for i in range(100):
-        A.append(random.randrange(100))
-    print("A:", A)
-    import insercion  #para verificar el resultado, no contarlo en el tiempo de este
-    print("Aord:", insercion.ordenar(A))  #A sigue desordenada
     B = [-1] * 1000  # arreglo "vacio", -1 = espacio vacío
-    for a in A:
+    for a in A[:100]:
         desp = 0
         "busca un espacio vacío..."
         while B[10 * a + desp] != -1:
@@ -50,6 +51,11 @@ def algoritmo2():
             print(b, end=", ")
     print()
 
-
-
-algoritmo2()
+print("Algoritmo 2")
+A = []
+for i in range(100):
+    A.append(random.randrange(100))
+print("A:", A)
+import insercion  #para verificar el resultado, no contarlo en el tiempo de este
+print("Aord:", insercion.ordenar(A))  #A sigue desordenada
+algoritmo2(A)
