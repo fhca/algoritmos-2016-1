c64a299836c8f62048b08390d20bfa325a5dc8e4
Felipe Contreras <hobber.mallow@gmail.com>
2016-01-26 11:05:08 -0600

    Mapa mental, descarga Freemind para verlo (muy simple todavía)

---
+++ docs/Análisis de Algoritmos.mm
@@ -0,0 +1,160 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1453422949106" ID="ID_1918272384" MODIFIED="1453422957654" TEXT="An&#xe1;lisis de Algoritmos">
+<node CREATED="1453423051701" ID="ID_276067917" LINK="Programa%20-%20Protocolo.pdf" MODIFIED="1453423475606" POSITION="left" TEXT="PROGRAMA">
+<icon BUILTIN="info"/>
+<node CREATED="1453422959927" ID="ID_1298044271" MODIFIED="1453736229547" TEXT="An&#xe1;lisis de eficiencia de los algoritmos">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <div title="Page 2" class="page">
+      <div class="section">
+        <div class="layoutArea">
+          <div class="column">
+            <ol>
+              <li>
+                <font face="Arial" size="2">Cotas inferiores y superiores </font>
+              </li>
+              <li>
+                <font face="Arial" size="2">Complejidad promedio</font>
+              </li>
+              <li>
+                <font face="Arial" size="2">Notacio&#769;n &#8220;O grande</font><font face="Arial">&#8221; </font>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1453422972404" ID="ID_1143276752" MODIFIED="1453691316116" TEXT="Algoritmos aritm&#xe9;ticos y algebraicos">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <div title="Page 2" class="page">
+      <div class="section">
+        <div class="layoutArea">
+          <div class="column">
+            <ol>
+              <li>
+                <font size="2" face="Arial">Complejidad en tiempo y en espacio</font>
+              </li>
+              <li>
+                <font size="2" face="Arial">Problemas P, NP y NP-completos </font>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html></richcontent>
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1453422987355" ID="ID_1168266063" MODIFIED="1453691309172" TEXT="Algoritmos de b&#xfa;squeda y ordenamiento">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <div title="Page 2" class="page">
+      <div class="section">
+        <div class="layoutArea">
+          <div class="column">
+            <ol>
+              <li>
+                <font size="2" face="Arial">Bu&#769;squeda lineal y binaria</font>
+              </li>
+              <li>
+                <font size="2" face="Arial">Ordenamiento directo: seleccio&#769;n e insercio&#769;n directas</font>
+              </li>
+              <li>
+                <font size="2" face="Arial">Ordenamiento eficiente: shellsort y quicksort </font>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html></richcontent>
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1453422990489" ID="ID_72842972" MODIFIED="1453691302718" TEXT="Algoritmos de gr&#xe1;ficas">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <div title="Page 3" class="page">
+      <div class="section">
+        <div class="layoutArea">
+          <div class="column">
+            <ol>
+              <li>
+                <font size="2" face="Arial">Representacio&#769;n en computadora </font>
+              </li>
+              <li>
+                <font size="2" face="Arial">Obtencio&#769;n del nu&#769;mero croma&#769;tico </font>
+              </li>
+              <li>
+                <font size="2" face="Arial">A&#769;rbol generador </font>
+              </li>
+              <li>
+                <font size="2" face="Arial">Nu&#769;mero de independencia. </font>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html></richcontent>
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1453423004387" ID="ID_1917129851" MODIFIED="1453691291583" TEXT="T&#xe9;cnicas de dise&#xf1;o de algoritmos">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <div title="Page 3" class="page">
+      <div class="section">
+        <div class="layoutArea">
+          <div class="column">
+            <ol>
+              <li>
+                <font size="2" face="Arial">Divide y vencera&#769;s </font>
+              </li>
+              <li>
+                <font size="2" face="Arial">Te&#769;cnicas &#8220;glotonas&#8221; </font>
+              </li>
+              <li>
+                <font size="2" face="Arial">Genera y prueba </font>
+              </li>
+              <li>
+                <font size="2" face="Arial">Vuelta atra&#769;s automa&#769;tico backtracking) </font>
+              </li>
+              <li>
+                <font size="2" face="Arial">Montecarlo </font>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html></richcontent>
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+</node>
+</map>
