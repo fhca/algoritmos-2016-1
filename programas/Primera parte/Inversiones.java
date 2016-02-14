8830bde384de3e3e9af06c156577094c9b72f288
Felipe Contreras <hobber.mallow@gmail.com>
2016-02-13 18:36:37 -0600

    archivar

--- programas/Primera parte/Inversiones.java
+++ programas/Primera parte/Inversiones.java
@@ -1,10 +1,15 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 
 public class Inversiones {
-	public class Inversion {
-		public int i, j;
-		public Inversion(int i, int j){
+	
+	// Parejas de enteros, usados para reportar inversiones (i,j) y parejas (n,pasos) 
+	public class Pareja {
+		int i, j;
+		public Pareja(int i, int j){
 			this.i = i;
 			this.j = j;
 		}
@@ -12,31 +17,53 @@ public class Inversiones {
 			return "(" + i + ", " + j + ")";
 		}
 	}
-	ArrayList<Inversion> reporta;
+	
+	ArrayList<Pareja> reporta;
 	int ary[];
+	
 	public Inversiones(int ary[]){
 		this.ary = ary;
 	}
-	ArrayList<Inversion> go_cuadratico(){
-		reporta = new ArrayList<Inversion>();
-		for(int i = 0; i < ary.length; i ++){
-			for(int j = i + 1; j < ary.length; j ++){
-				if (ary[i] > ary[j]){
-					reporta.add(new Inversion(i,j));
-				}
+	
+	// Algoritmo cuadrático para encontrar las inversiones en una lista de enteros
+	ArrayList<Pareja> go_cuadratico(){
+		int pasos = 0;  // devolver esto para construir la gráfica
+		reporta = new ArrayList<Pareja>();
+		for(int i = 0; i < ary.length; i ++)
+			for(int j = i + 1; j < ary.length; j ++) {
+				if (ary[i] > ary[j])
+					reporta.add(new Pareja(i,j));
+				pasos ++;
 			}
-		}
 		return reporta;
 	}
-	ArrayList<Inversion> go_nlogn(){
-		reporta = new ArrayList<Inversion>();
+	
+	// Algoritmo nlogn para encontrar las inversiones en una lista de enteros
+	ArrayList<Pareja> go_nlogn(){
+		int pasos = 0;  // devolver esto para construir la gráfica
+		reporta = new ArrayList<Pareja>();
 		// aquí va la modificación a merge_sort
 		return reporta;
+	}
+	
+	// imprime las Parejas n,pasos en un archivo
+	static void archivar(String arch, ArrayList<Pareja> lista){
+		try {
+			PrintStream out = new PrintStream(new FileOutputStream(arch));
+			for (int i = 0; i < lista.size(); i++){
+				Pareja p = lista.get(i);
+				out.println(p.i + " " + p.j);
+			}
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
 		int a [] = {2, 3, 8, 6, 1};
 		Inversiones inv = new Inversiones(a);
 		System.out.println(inv.go_cuadratico());
+		//archivar("prueba.txt", inv.go_cuadratico());
 	}
 }
